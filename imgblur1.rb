5bab4ff1b5f2b3e31c02af0c455a848a9ee662b2
Liz Gehret <elizabeth.gehret@gmail.com>
2019-10-02 04:54:28 +0000

    imgblur1-final

---
+++ imgblur1.rb
@@ -0,0 +1,22 @@
+
+class Image
+  def initialize(pixels)
+    @pixels = pixels
+  end
+
+  def output_image
+    @pixels.each do |pixels|
+      puts pixels.join
+    end
+  end
+
+end
+
+#creating an instance of Image class
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+ image.output_image
