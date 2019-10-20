c095e0b506d4bf596a028a39fd651360b9775e08
Liz Gehret <elizabeth.gehret@gmail.com>
2019-10-20 22:14:21 +0000

    Updates to imgblur2

--- imgblur2.rb
+++ imgblur2.rb
@@ -10,35 +10,22 @@ class Image
     end
   end
 
-  def image_blur
-    coords = []
-
-    @pixels.each_with_index do |row, x|
-      row.each_with_index do |column, y|
-        if column == 1
-          coords << [x, y]
-        end
-      end
-    end
-
-    #puts coords.inspect
-
     #each_with_index takes two arguments: the value and the index
     #since this is an array of arrays, the first value set is the row, and it's corresponding x value
     #the second value set is the actual value in the row, and it's corresponding y value
+  def blur_coords
+    blur_coords = []
     @pixels.each_with_index do |row, x|
       row.each_with_index do |column, y|
-        coords.each do |x_coord, y_coord|
-          #puts String(x_coord) << " " << String(y_coord) << "..." << String(x) << " " << String(y)
-          if x_coord == x && y_coord == y
-            @pixels[row[x]+1][column[y]] = 1 if row[x] + 1 <= @pixels[row[x]].length - 1
-            @pixels[row[x]-1][column[y]] = 1 if row[x] - 1 >= 0
-            @pixels[row[x]][column[y]+1] = 1 if column[y] + 1 <= @pixels[column[y]].length - 1
-            @pixels[row[x]][column[y]-1] = 1 if column[y] - 1 >= 0
-            puts String(x) << " " << String(y)
-          end
-        end
+        blur_coords << [x, y] if column == 1
       end
+    end
+
+    blur_coords.each do |coord|
+      @pixels[coord[0]][coord[1]+1] = 1 if coord[1] + 1 <= @pixels[coord[0]].length - 1
+      @pixels[coord[0]][coord[1]-1] = 1 if coord[1] - 1 >= 0
+      @pixels[coord[0]+1][coord[1]] = 1 if coord[0] + 1 <= @pixels.length - 1
+      @pixels[coord[0]-1][coord[1]] = 1 if coord[0] - 1 >= 0
     end
   end
 end
@@ -51,5 +38,10 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
- image.output_image
- image.image_blur
+puts "Original Image"
+image.output_image
+
+puts "Blurred Image"
+image.blur_coords
+image.output_image
+
